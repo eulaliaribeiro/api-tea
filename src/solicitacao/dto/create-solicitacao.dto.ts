9c9998b8691253c62577d324efe5dbb79ec18d37
Raphael Goncalves Silva <raphaelatuario@gmail.com>
2022-11-15 00:10:03 -0300

    Criação dos DTOS de endereço, solicitação e terapia solicitada

--- src/solicitacao/dto/create-solicitacao.dto.ts
+++ src/solicitacao/dto/create-solicitacao.dto.ts
@@ -1,1 +1,19 @@
-export class CreateSolicitacaoDto {}
+import {IsAlpha, IsDate, IsNotEmpty, IsNumber, IsNumberString, IsOptional, IsPositive, IsString, Length, length, max, ValidateNested } from 'class-validator'
+import {Transform, Type} from'class-transformer';
+import { TerapiaSolicitada } from 'src/terapia-solicitada/entities/terapia-solicitada.entity';
+
+export class CreateSolicitacaoDto {
+
+@IsDate({message:"Dados invalida"})
+@IsNotEmpty({message:"Data da solicitacao obrigatória"})
+@Type(()=>Date)
+datasolicitacao:Date;
+
+
+@IsNotEmpty({message:"Nome obrigatório"})
+@Length(0,60,{message:"O tamanho deve ser 0 e 60"})
+@IsString({message:"Deve ser String"})
+@Transform(({ value }) => value.toUpperCase())
+medicosolicitante: String;
+
+}
--- src/terapia-solicitada/dto/create-terapia-solicitada.dto.ts
+++ src/terapia-solicitada/dto/create-terapia-solicitada.dto.ts
@@ -1,1 +1,27 @@
-export class CreateTerapiaSolicitadaDto {}
+import {IsAlpha, IsDate, IsNotEmpty, IsNumber, IsNumberString, IsOptional, IsPositive, IsString, Length, length, max, ValidateNested } from 'class-validator'
+import {Transform, Type} from'class-transformer';
+import { TerapiaSolicitada } from 'src/terapia-solicitada/entities/terapia-solicitada.entity';
+
+export class CreateTerapiaSolicitadaDto {
+
+
+@IsNotEmpty({message:"Nome da terapia é obrigatório"})
+@Length(1,100,{message:"O tamanho deve ser 1 e 100"})
+@IsString({message:"Deve ser String"})
+@Transform(({ value }) => value.toUpperCase())
+nome: String;
+
+
+@IsNotEmpty({message:"Nome da metodo é obrigatório"})
+@Length(1,20,{message:"O tamanho deve ser 1 e 20"})
+@IsString({message:"Deve ser String"})
+@Transform(({ value }) => value.toUpperCase())
+metodoTerapeutico: String;
+
+
+@IsNotEmpty({message:"Numero das sessões é obrigatório"})
+@IsPositive()
+qtdsessoes :number
+
+
+}
